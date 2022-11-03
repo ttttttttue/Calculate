6f86ebe2c2144d859193d98a1460ccb103efe80a
Anton <Krasikov-94@bk.ru>
2022-11-03 18:36:38 +0300

    Initial Commit

---
+++ Calculate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculate
+//
+//  Created by Антон Красиков on 03.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
